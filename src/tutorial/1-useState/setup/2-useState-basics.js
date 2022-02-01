45e8a452d715b82b9a4c8a6ce3ca6eedd2f9f4b5
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-01 22:28:41 +0600

    learnt basics of useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // const value = useState("hello World");
+  // console.log(useState(value))
+
+  const[text,setText]=useState("Click Button")
+  const handleChange =()=>{
+    if(text==='Click Button'){
+      setText("'This Button Has been Clicked'");
+    }else{
+      setText("Click Button")
+    }
+  }
+  return (
+    <div>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleChange}>
+        {text}
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
