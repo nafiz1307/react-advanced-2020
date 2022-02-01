fd09aefdd00215ac4dbe062dfcd4753f7f36993a
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-01 22:47:53 +0600

    useState Array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from '../src/tutorial/1-useState/setup/2-useState-basics';
+import Setup from '../src/tutorial/1-useState/setup/3-useState-array'
 ;
 
 function App() {
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
+import { useEffect, useState } from "react";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem=(id)=>{
+    let newPeople = people.filter((person)=>person.id!==id)
+    setPeople(newPeople)
+  }
+
+  return (
+    <div>
+      {people.map((person) => {
+        const{id,name}=person
+        return <div key={id} className="item">
+          <h4>{name}</h4>
+          <button onClick={()=>removeItem(id)}> Remove </button>
+        </div>
+      })}
+      {/* <button className="btn" onClick={()=>setPeople([])}>Clear Items</button> */}
+    </div>
+  );
 };
 
 export default UseStateArray;
