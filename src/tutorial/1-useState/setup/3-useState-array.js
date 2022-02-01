6efa488925265f5776608e351aacbf1084e781a4
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-01 23:09:28 +0600

    sort object based on id

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,21 +4,40 @@ import { useEffect, useState } from "react";
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
-  const removeItem=(id)=>{
-    let newPeople = people.filter((person)=>person.id!==id)
-    setPeople(newPeople)
+  console.log(people);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAll = () => {
+    setPeople([]);
+  };
+
+  const sortPeople=()=>{
+    let tempArray = [...people];
+    tempArray.sort((a, b) => b.id-a.id);
+    console.log(tempArray)
+    setPeople(tempArray);
   }
 
   return (
     <div>
       {people.map((person) => {
-        const{id,name}=person
-        return <div key={id} className="item">
-          <h4>{name}</h4>
-          <button onClick={()=>removeItem(id)}> Remove </button>
-        </div>
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}> Remove </button>
+          </div>
+        );
       })}
       {/* <button className="btn" onClick={()=>setPeople([])}>Clear Items</button> */}
+      <button className="btn" onClick={clearAll}>
+        Clear All
+      </button>
+      <button className="btn" onClick={sortPeople}>
+        Sort
+      </button>
     </div>
   );
 };
